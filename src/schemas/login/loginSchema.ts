177e786250df289e4b7966e217d9e141a1115c95
kimura <shibaduke4@gmail.com>
2025-04-12 02:36:10 +0900

    ⚡️ 空文字修正

--- src/schemas/login/loginSchema.ts
+++ src/schemas/login/loginSchema.ts
@@ -4,8 +4,10 @@ import { z } from "zod";
 export const loginSchema = z.object({
   email: z
     .string()
+    .min(1, { message: "有効なメールアドレスを入力してください。" })
     .email({ message: "有効なメールアドレスを入力してください。" }),
   password: z
     .string()
+    .min(1, { message: "パスワードは6文字以上で入力してください。" })
     .min(6, { message: "パスワードは6文字以上で入力してください。" }),
 });
